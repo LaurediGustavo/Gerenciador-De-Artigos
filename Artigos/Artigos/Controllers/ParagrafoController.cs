37bcccee3e6fdb0c451a4f2ece22dde5f6654d38
Lauredi Gustavo <68924576+LaurediGustavo@users.noreply.github.com>
2021-08-20 10:55:49 -0300

    Commit telaParagrafo

--- Artigos/Artigos/Controllers/ParagrafoController.cs
+++ Artigos/Artigos/Controllers/ParagrafoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Artigos.Models;
+using Microsoft.AspNet.Identity;
 
 namespace Artigos.Controllers
 {
@@ -11,9 +12,55 @@ namespace Artigos.Controllers
     public class ParagrafoController : Controller
     {
         private readonly Context db = new Context();
-        public ActionResult Index()
+        
+        [HttpGet]
+        public ActionResult Index(int id)
         {
+            ViewBag.Id = id;
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetAll(int id)
+        {
+            int idUser = int.Parse(User.Identity.GetUserId().ToString());
+            Artigo artigo = db.Artigos.Find(id);
+
+            if(idUser == artigo.EscritorId || User.IsInRole("Administrador"))
+            {
+                var artigosId = (from p in db.Paragrafos
+                                 where p.ArtigoId == id
+                                 select new
+                                 {
+                                     p.Id
+                                 }).ToList();
+
+                return Json(artigosId, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json("");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(int id, string texto)
+        {
+            int idUser = int.Parse(User.Identity.GetUserId().ToString());
+            Artigo artigo = db.Artigos.Find(id);
+
+            if ((idUser == artigo.EscritorId || User.IsInRole("Administrador")) && !String.IsNullOrEmpty(texto))
+            {
+                db.Paragrafos.Add(new Paragrafo() 
+                {  
+                    ArtigoId = id,
+                    Texto = texto
+                });
+                db.SaveChanges();
+
+                return Json("Paragrafo adicionado!");
+            }
+
+            return Json("");
         }
     }
 }
---
+++ Artigos/Artigos/Scripts/Paragrafo.js
@@ -0,0 +1,76 @@
+﻿function GetAll(id) {
+    $.ajax({
+        url: "/Paragrafo/GetAll/" + id,
+        contentType: "application/json; charset=utf-8",
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+            var html = '';
+            var num = 1;
+            $.each(result, function (key, item) {
+                html += '<a onclick="Details(' + item.Id + ')">';
+                html += '<div class="divLista">';
+                html += 'Paragrafo ' + num;
+                html += '</div>';
+                html += '</a>';
+                num++;
+            });
+
+            $('#listaParagrafo').html(html);
+        },
+        error: function () {
+            alert("Erro ao buscar os paragrafos!");
+        }
+    });
+}
+
+function Create(id) {
+    if ($('#texto').val() != '') {
+        $('#erro').text('');
+
+        var form = $('#__AjaxAntiForgeryForm');
+        var token = $('input[name="__RequestVerificationToken"]', form).val();
+        var texto = $('#texto').val();
+
+        $.ajax({
+            url: "/Paragrafo/Create/" + id,
+            contentType: "application/x-www-form-urlencoded;charset=utf-8",
+            type: "POST",
+            dataType: "json",
+            data: {
+                __RequestVerificationToken: token,
+                texto
+            },
+            success: function (result) {
+                alert("Paragrafo adicionado!");
+                $('#texto').val('');
+                GetAll(id);
+            },
+            error: function () {
+                alert("Erro ao adicionar!");
+            }
+        });
+    }
+    else {
+        $('#erro').text('Esse campo é obrigatório!');
+    }
+}
+
+function Details(id) {
+    $.ajax({
+        url: "/Paragrafo/Details/" + id,
+        contentType: "application/json; charset=utf-8",
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+
+        },
+        error: function () {
+            alert("Erro ao buscar o paragrafo!");
+        }
+    });
+}
+
+function DetailsImg() {
+
+}
